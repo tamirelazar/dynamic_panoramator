35aa787d63a31a86642ad20e503fbfda01878c99
Tamir Elazar <60173095+tamirelazar@users.noreply.github.com>
2021-04-11 16:16:24 +0300

    Update make_panorama.py

--- make_panorama.py
+++ make_panorama.py
@@ -1,5 +1,5 @@
 import os
-import sol4
+import dynamic_panoramator
 import time
 
 def main():
@@ -12,7 +12,7 @@ def main():
     os.system('ffmpeg -i videos/%s dump/%s/%s%%03d.jpg' % (experiment, exp_no_ext, exp_no_ext))
 
     s = time.time()
-    panorama_generator = sol4.PanoramicVideoGenerator('dump/%s/' % exp_no_ext, exp_no_ext, 2100)
+    panorama_generator = dynamic_panoramator.PanoramicVideoGenerator('dump/%s/' % exp_no_ext, exp_no_ext, 2100)
     panorama_generator.align_images(translation_only=True)
     panorama_generator.generate_panoramic_images(9)
     print(' time for %s: %.1f' % (exp_no_ext, time.time() - s))
